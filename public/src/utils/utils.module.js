2360b858fb3e0c91be782c8d980fdee1e42fda1b
Tim Consolazio <tcoz@tcoz.com>
2017-01-04 20:17:07 -0500

    Initial debouncer rxjs and viewcomponent

---
+++ public/src/utils/utils.module.js
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+class UtilsModule {
+    constructor () {};
+}
+
+UtilsModule.annotations = [
+    new NgModule ( {
+        declarations: [ ]
+    })
+];
+
+export { UtilsModule }
